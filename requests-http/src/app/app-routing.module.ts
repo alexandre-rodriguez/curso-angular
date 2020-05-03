1087432fc0b533fde8fbfa81e9254923f82f8e94
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-05-03 16:11:09 -0300

    Aula 124 - Capturando Erros (+Erro com async)

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'rxjs-poc',
+    redirectTo: 'cursos',
   },
   {
     path: 'cursos',
--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -12,11 +13,12 @@ import { Observable } from 'rxjs';
 })
 export class CursosListaComponent implements OnInit {
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) {}
 
   ngOnInit(): void {
-    this.cursos$ = this.service.list();
+    this.onRefresh();
   }
 
   onDeclineDelete() {}
@@ -27,5 +29,13 @@ export class CursosListaComponent implements OnInit {
 
   onEdit(i: number) {}
 
-  onRefresh() {}
+  onRefresh() {
+    this.cursos$ = this.service.list().pipe(
+      catchError((error) => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+  }
 }
