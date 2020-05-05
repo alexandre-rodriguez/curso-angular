d5894d215ba4d183ce6003226e80e4c25f84d4e9
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-05-05 20:07:50 -0300

    Aula 140 - Criando operador RxJS customizado

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { UploadFileService } from './../upload-file.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -39,18 +40,14 @@ export class UploadFileComponent implements OnInit {
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, '/api/upload')
-        .subscribe((event: HttpEvent<Object>) => {
-          console.log(event);
-          if (event.type === HttpEventType.Response) {
-            console.log('Upload Concluído');
-          } else if (event.type === HttpEventType.UploadProgress) {
-            const percentDone = Math.round(event.loaded * 100 / event.total);
-            console.log('Progresso', percentDone);
-            this.progress = percentDone;
-          }
-
-
-        });
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluído'));
     }
   }
 }
